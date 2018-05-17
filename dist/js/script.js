f37ec5d869ce8172dae19bc9e1c5dec65a2da0cd
Igor Korenets <vik753@gmail.com>
2018-05-17 14:01:57 +0300

    work done

--- dist/js/script.js
+++ dist/js/script.js
@@ -916,8 +916,8 @@ $(document).ready(function () {
         loop: true,
         center: true,
         dots: true,
-        autoplay: false,
-        autoplayTimeout: 10000
+        autoplay: true,
+        autoplayTimeout: 6000
     });
 });
 
@@ -955,9 +955,23 @@ jQuery(document).ready(function () {
         offset: 100,
         removeClassAfterAnimation: true
     });
+
+    jQuery('.pc-context__button-wrapper').addClass("hidden").viewportChecker({
+        classToRemove: 'hidden',
+        classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
+        offset: 100,
+        removeClassAfterAnimation: true
+    });
+
+    jQuery('.pc-context__block').addClass("hidden").viewportChecker({
+        classToRemove: 'hidden',
+        classToAdd: 'visible animated bounceInUp', // Class to add to the elements when they are visible
+        offset: 100,
+        removeClassAfterAnimation: true
+    });
 });
 
-//===================tabs turn on/off==
+//===================owl slider tabs turn on/off==
 
 $('.slide__button').click(function (e) {
     e.preventDefault();
@@ -971,5 +985,25 @@ $('.slide__button').click(function (e) {
     } else {
         $('.slide__text1').hide();
         $('.slide__text2').show();
+    }
+});
+
+//===================pc context tabs turn on/off==
+$('.pc-context__button').addClass('greyFont');
+
+$('.pc-context__button').click(function (e) {
+    e.preventDefault();
+
+    $(this).parent().children().removeClass('blueFont');
+    $(this).parent().children().addClass('greyFont');
+    $(this).removeClass('greyFont');
+    $(this).addClass('blueFont');
+
+    if ($(this).text() === 'Smart design') {
+        $('.pc-context__block_tab1').show();
+        $('.pc-context__block_tab2').hide();
+    } else {
+        $('.pc-context__block_tab1').hide();
+        $('.pc-context__block_tab2').show();
     }
 });
